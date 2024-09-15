ff8964a501f29932bdae128097d9fd78101dfc92
tchoforwilson <tchoforwilson@gmail.com>
2024-09-15 20:36:32 +0100

    Getters and setters

--- classes/com/codewithdev/Employee.java
+++ classes/com/codewithdev/Employee.java
@@ -1,8 +1,28 @@
 package com.codewithdev;
 
 public class Employee {
-    public int baseSalary;
+    private int baseSalary;
     public int hourlyRate;
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less.");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return this.baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate must be greater than 0");
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHourlRate() {
+        return this.hourlyRate;
+    }
 
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
