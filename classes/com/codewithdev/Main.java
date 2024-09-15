129084b8eee460c641b1c70274d261e680f439f5
tchoforwilson <tchoforwilson@gmail.com>
2024-09-15 21:18:38 +0100

    Reducing coupling

--- classes/com/codewithdev/Main.java
+++ classes/com/codewithdev/Main.java
@@ -2,12 +2,8 @@ package com.codewithdev;
 
 public class Main {
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.setBaseSalary(50_000);
-        employee.setHourlyRate(20);
-
-        int wage = employee.calculateWage(10);
-        System.out.println(wage);
+        var browser = new Browser();
+        browser.navigate(null);
     }
 
 }
