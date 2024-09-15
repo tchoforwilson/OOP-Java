a3ea19ade1231f1ddefbffc0b7a1f45ada440f33
tchoforwilson <tchoforwilson@gmail.com>
2024-09-15 20:24:03 +0100

    Encapsulation

---
+++ classes/com/codewithdev/Employee.java
@@ -0,0 +1,11 @@
+package com.codewithdev;
+
+public class Employee {
+    public int baseSalary;
+    public int hourlyRate;
+
+    public int calculateWage(int extraHours) {
+        return baseSalary + (hourlyRate * extraHours);
+    }
+
+}
