6cff19a638cec777fc7dfb11a5811c56d2dc69a8
tchoforwilson <tchoforwilson@gmail.com>
2024-09-15 20:10:00 +0100

    Memory allocation

--- classes/com/codewithdev/Main.java
+++ classes/com/codewithdev/Main.java
@@ -3,11 +3,8 @@ package com.codewithdev;
 public class Main {
     public static void main(String[] args) {
         var textBox1 = new TextBox();
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text.toUpperCase());
-
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text);
+        var textBox2 = textBox1;
+        textBox2.setText("Hello, world");
+        System.out.println(textBox1.text);
     }
 }
