e96bed9ffb8053b7705a93f8407fb4a6a7c28e6e
tchoforwilson <tchoforwilson@gmail.com>
2024-09-15 20:05:35 +0100

    Objects

---
+++ classes/com/codewithdev/Main.java
@@ -0,0 +1,13 @@
+package com.codewithdev;
+
+public class Main {
+    public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text.toUpperCase());
+
+        var textBox2 = new TextBox();
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text);
+    }
+}
