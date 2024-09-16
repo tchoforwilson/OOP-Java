7b66d6cb035b2b3e85e0ca0d862adafda95c84f3
tchoforwilson <tchoforwilson@gmail.com>
2024-09-16 21:33:23 +0100

    Last touch on refactoring

--- refactor/com/codewithdev/MortgageReport.java
+++ refactor/com/codewithdev/MortgageReport.java
@@ -1,16 +1,20 @@
+package com.codewithdev;
+
 import java.text.NumberFormat;
 
 public class MortgageReport {
 
+    private final NumberFormat currency;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        String mortgageFormatted = currency.format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
