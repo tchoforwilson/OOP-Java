fe5e1f434fcb8d855eda04debd2738d6c00bc9e0
tchoforwilson <tchoforwilson@gmail.com>
2024-09-21 10:58:56 +0100

    Abstract Classes and Methods

--- inheritance/com/codewithdev/UIControl.java
+++ inheritance/com/codewithdev/UIControl.java
@@ -1,14 +1,13 @@
 package com.codewithdev;
 
-public class UIControl {
+public abstract class UIControl {
     private boolean isEnabled = true;
 
     // public UIControl(boolean isEnabled) {
     // this.isEnabled = isEnabled;
     // System.out.println("UIControl");
     // }
-    public void render() {
-    }
+    public abstract void render();
 
     public void enable() {
         isEnabled = true;
