d18d8b1558f83f1c93e6640698dbc35e6166c2fe
tchoforwilson <tchoforwilson@gmail.com>
2024-09-22 13:04:42 +0100

    Setter Injection

--- interfaces/com/codewithdev/Main.java
+++ interfaces/com/codewithdev/Main.java
@@ -6,5 +6,8 @@ public class Main {
         var calculator = new TaxCalculator2018(100_000);
         var report = new TaxReport(calculator);
         report.show();
+
+        report.setCalculator(new TaxCalculator2019());
+        report.show();
     }
 }
---
+++ interfaces/com/codewithdev/TaxCalculator2019.java
@@ -0,0 +1,10 @@
+package com.codewithdev;
+
+public class TaxCalculator2019 implements TaxCalculator {
+
+    @Override
+    public double calculateTax() {
+        return 0;
+    }
+
+}
