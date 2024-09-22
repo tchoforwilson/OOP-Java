2236cf8ddfe97a4ab5ae140b48371aeabd31adc5
tchoforwilson <tchoforwilson@gmail.com>
2024-09-22 13:08:37 +0100

    Method Injection

--- interfaces/com/codewithdev/Main.java
+++ interfaces/com/codewithdev/Main.java
@@ -4,10 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         var calculator = new TaxCalculator2018(100_000);
-        var report = new TaxReport(calculator);
-        report.show();
+        var report = new TaxReport();
+        report.show(calculator);
 
-        report.setCalculator(new TaxCalculator2019());
-        report.show();
+        report.show(new TaxCalculator2019());
     }
 }
